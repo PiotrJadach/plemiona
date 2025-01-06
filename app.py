8df04bfe20822a95a6cb2969ec782d801d7c639e
Piotr <149265019+PiotrJadach@users.noreply.github.com>
2025-01-06 13:18:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+import psycopg2
+import configparser
+from flask import Flask, jsonify
+
+# Dane do połączenia z bazą danych
+def connect_to_db(config_file='config.ini'):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    params = config['POST']
+    conn = psycopg2.connect(**params)
+    return conn
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def home():
-    return "<h1>Arpan's Flask App</h1>"
+@app.route('/data')
+def get_data():
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM dzienne_porownanie")
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(results)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
